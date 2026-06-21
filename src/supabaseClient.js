776a489576370dbcaf117acdb1aef2df39610771
nkhp7s5rwd-dotcom <nkhp7s5rwd@privaterelay.appleid.com>
2026-06-21 15:03:32 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,6 @@
-tt
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "DEINE_SUPABASE_URL";
+const supabaseAnonKey = "DEIN_ANON_KEY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
